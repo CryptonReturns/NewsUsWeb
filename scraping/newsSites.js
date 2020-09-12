4d45f0d5cb8830c6e6e327ce16948e2b77c7b661
Rohan Jain <rohan19095@iiitd.ac.in>
2020-09-12 23:08:11 +0530

    Formatting

--- scraping/newsSites.js
+++ scraping/newsSites.js
@@ -1,19 +1,26 @@
 const sites = {
-    "finance":[
-        "https://www.financialexpress.com/", "https://economictimes.indiatimes.com/", "https://www.moneycontrol.com/" //Any 2
-    ],
-    "technology": [
-        "https://gadgets.ndtv.com/", "https://www.cnet.com/"
-    ],
-    "general": [
-        "https://timesofindia.indiatimes.com/us", "https://www.thehindu.com/", "https://www.hindustantimes.com/" //any 2
-    ],
-    "entertainment": [
-        "", //Need to find
-    ],
-    "international": [
-        "https://www.aljazeera.com/", "https://www.bbc.com/"
-    ]
-}
+  finance: [
+    "https://www.financialexpress.com/",
+    "https://economictimes.indiatimes.com/",
+    "https://www.moneycontrol.com/", //Any 2
+  ],
+  technology: ["https://gadgets.ndtv.com/", "https://www.cnet.com/"],
+  general: [
+    "https://timesofindia.indiatimes.com/us",
+    "https://www.thehindu.com/",
+    "https://www.hindustantimes.com/", //any 2
+  ],
+  entertainment: [
+    "", //Need to find
+  ],
+  international: ["https://www.aljazeera.com/", "https://www.bbc.com/"],
+};
 //Add more categories
 module.exports = sites;
+
+// bbc.com
+// timesofindia
+// economictiems may be similar to toi because both from indiatimes
+// financial express
+// nytimes(Entertainment)
+// indianexpress
