f4395f175bdc2529f32dbe49ee9d21f2ddc31ea0
sksum <sakshammrigdgr8@gmail.com>
2020-09-12 11:29:04 +0530

    initReact

---
+++ newsus/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MainComponent from './Components/MainComponent.jsx'
+
+function App() {
+  return (
+    <div >
+        <MainComponent />
+    </div>
+  );
+}
+
+export default App;
